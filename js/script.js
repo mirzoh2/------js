8bb20dcb1e38a0f4a2ad174cb5c83b6587e9bf26
mirzoh2 <mirzomirbadalov357@gmail.com>
2023-02-26 22:21:00 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let doc = document//Обращаюсь к странице
+// let first_child = doc.querySelector('div')//Достаю бокс
+
+// for(let i of first_child.children){
+//     console.log(i);
+// }
+
+// first_child.style.display = 'flex'//Добавляю стили
+// first_child.style.alignItems = 'center'
+// first_child.style.justifyContent = 'center'
+// first_child.style.gap = '20px'
+
+// console.log(first_child);

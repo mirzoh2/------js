b5f07b3855f40b70344b4db97b10eb7e139bbeb1
mirzoh2 <mirzomirbadalov357@gmail.com>
2023-03-12 00:06:48 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -106,6 +106,7 @@ var swiper = new Swiper(".mySwiper", {
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
         navigation: {
       nextEl: '.swiper-button-next',
